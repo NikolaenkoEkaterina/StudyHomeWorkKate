f0b9cf1047528a06bcb0d271ec982e0b8c4c3d57
katherine.pash <katherine.pash@mail.ru>
2024-05-09 19:48:44 +0300

    HomeWork-09.05.2024, create new s

--- study/src/main/java/com/example/study/HomeWork1/Task1.java
+++ study/src/main/java/com/example/study/HomeWork1/Task1.java
@@ -3,5 +3,6 @@ package com.example.study.HomeWork1;
 public class Task1 {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("I am here");
     }
 }
